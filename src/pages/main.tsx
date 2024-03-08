65f57f0bab30db2d99024c1e50135628b07bab29
mmmihhaeel <mikael11032005@gmail.com>
2024-03-08 11:41:14 +0300

    updated README.md file

--- src/pages/main.tsx
+++ src/pages/main.tsx
@@ -1,6 +1,8 @@
 import React, { FC } from "react";
 
 const MainPage: FC = () => {
+    console.log(import.meta.env.AUTH_API_URL, import.meta.env.REDIRECT_URL);
+
     return <React.Fragment>
 
     </React.Fragment>;
